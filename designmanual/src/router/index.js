378870b1c8ae5347de49e81907cffe9d2f47b430
madsmads4 <madsolsen213@gmail.com>
2025-11-24 09:58:00 +0100

    Update index.js

--- designmanual/src/router/index.js
+++ designmanual/src/router/index.js
@@ -1,23 +1,85 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import HomeView from "../views/HomeView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'home',
+      path: "/",
+      name: "home",
       component: HomeView,
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
+      path: "/tableofcontent",
+      name: "tableofcontent",
+      component: () => import("../views/AboutView.vue"),
+    },
+    {
+      path: "/introduction",
+      name: "introduction",
+      component: () => import("../views/AboutView.vue"),
+    },
+    {
+      path: "/setup-installation",
+      name: "setup-installation",
+      component: () => import("../views/AboutView.vue"),
+    },
+    {
+      path: "/setup-structure",
+      name: "setup-structure",
+      component: () => import("../views/AboutView.vue"),
+    },
+    {
+      path: "/setup-firebase",
+      name: "setup-firebase",
+      component: () => import("../views/AboutView.vue"),
+    },
+    {
+      path: "/setup-framework",
+      name: "setup-framework",
+      component: () => import("../views/AboutView.vue"),
+    },
+    {
+      path: "/manage-component",
+      name: "manage-component",
+      component: () => import("../views/AboutView.vue"),
+    },
+    {
+      path: "/manage-import",
+      name: "manage-import",
+      component: () => import("../views/AboutView.vue"),
+    },
+    {
+      path: "/manage-styling",
+      name: "manage-styling",
+      component: () => import("../views/AboutView.vue"),
+    },
+    {
+      path: "/manage-routing",
+      name: "manage-routing",
+      component: () => import("../views/AboutView.vue"),
+    },
+    {
+      path: "/manage-JSON",
+      name: "manage-JSON",
+      component: () => import("../views/AboutView.vue"),
+    },
+    {
+      path: "/manage-JSON-template",
+      name: "manage-JSON-template",
+      component: () => import("../views/AboutView.vue"),
+    },
+    {
+      path: "/maintenance-expand",
+      name: "maintenance-expand",
+      component: () => import("../views/AboutView.vue"),
+    },
+    {
+      path: "/troubleshooting",
+      name: "troubleshooting",
+      component: () => import("../views/AboutView.vue"),
     },
   ],
-})
+});
 
-export default router
+export default router;
