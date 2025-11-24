00e73f4821406e914500fc8e1f3c0f168defb15a
SofiePedersen <sofie@gefiber.dk>
2025-11-25 00:05:08 +0100

    Routing done

--- designmanual/src/views/ManageroutingView.vue
+++ designmanual/src/views/ManageroutingView.vue
@@ -9,7 +9,7 @@ import FooterComp from '@/components/FooterComp.vue';
     <div class="wrapper">
       <div class="text-wrapper">
         <h2>3.4 SPA-routing</h2>
-        <p>Projektet er opbygget som en Single Page Application (SPA), hvor navigationen håndteres af Vue Router.</p>
+        <p>Projektet er opbygget som en <b>Single Page Application (SPA)</b>, hvor navigationen håndteres af Vue Router.</p>
         <p>I stedet for at indlæse nye sider opdateres kun det område, hvor <<span class="color-change">RouterView</span> /> ligger i App.vue.</p>
         <p>Som intern retningslinje anvendes <<span class="color-change">RouterView</span> /> til al intern navigation mellem undersider, mens almindelige <<span class="color-change">a</span>>-<b>tags bruges kun til eksterne links</b>.</p>
         <p>Det sikrer korrekt SPA-adfærd og undgår unødige sideindlæsninger.</p>
@@ -17,8 +17,15 @@ import FooterComp from '@/components/FooterComp.vue';
       <div class="examples-wrapper">
         <h2>Eksempler fra vores kode:</h2>
         <p>Ruterne defineres i router/index.js og kobler en URL-sti til et specifikt view:</p>
-        <img class="" src="" alt="" />
+        <img class="examples-image-1" src="..\assets\image\manage-picture-7.webp" alt="Image of router paths" />
         <p>Når man så skal lave et <<span class="color-change">RouterLink </span>> vil det se sådan her ud</p>
+        <img class="examples-image-2" src="..\assets\image\manage-picture-8.webp" alt="Image of routerlinks" />
+        <p>Visningen vil ske automatisk da dit <<span class="color-change">RouterLink</span>> altid vil blive vist gennem vores <<span class="color-change">RouterLink</span>/> der altid vil befinde sig på App.vue siden.</p>
+        <img class="examples-image-3" src="..\assets\image\manage-picture-9.webp" alt="Image of routerlinks" />
+        <h2>Oprettelse af nye views</h2>
+        <p>Når der skal tilføjes en ny underside, oprettes først et view i views-mappen. Din sti kan f.eks. se således ud: <span class="color-change">/src/views/AboutView.vue</span></p>
+        <p>Derefter skal den registeres i routeren således:</p>
+        <img class="examples-image-4" src="..\assets\image\manage-picture-10.webp" alt="Image of new routes" />
       </div>
     </div>
   </main>
@@ -45,7 +52,16 @@ import FooterComp from '@/components/FooterComp.vue';
 
   .examples-wrapper {
     padding: 2rem;
+
+    p {
+      margin-bottom: 2rem;
+    }
+
+    .examples-image-3 {
+      margin-bottom: 2rem;
+    }
   }
+
 }
 
 
