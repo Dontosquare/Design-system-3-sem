17c5fafe4a3263fd7752c23b0acab21b367e15bf
Dontosquare <153940558+Dontosquare@users.noreply.github.com>
2025-11-24 14:48:24 +0100

    sweaugthalig

--- designmanual/src/views/ManageJSONView.vue
+++ designmanual/src/views/ManageJSONView.vue
@@ -5,21 +5,23 @@ import ManagePicture11 from "@/assets/image/manage-picture-11.webp";
 <template>
   <main class="main__overall">
     <div class="json__overall">
-      <h2>3.5 Dymamisk navigaiton via JSON</h2>
-      <p> Burgermenuernes navigation bygger på data hentet fra en JSON fil, som indeholder bl.a
-        navnene på de navigationsknapper, der vises i menuen.
-      </p>
-      <p>Når der skal foretages ændringer i menuen, kan man nøjes med at opdatere titlerne i de relevante arrays i JSON filerne</p>
-      <p>Selve Header-komponenten håndtere automatisk både struktur og styling, så ændringerne slår igennem  uden yderligere justeringer
-        i komponentkoden.
-      </p>
-      <p>Denne tilgang gør det både hurtigt og sikkert at vedligeholde navigationen, samtidig med at koden forbliver overskuelig og
-        genanvendelig.
-      </p>
-      <div class="json__img">
-        <img :src="ManagePicture11" alt="jsonimg" id="jsonimage"> 
+      <div class="json__text">
+        <h2>3.5 Dymamisk navigaiton via JSON</h2>
+        <p> Burgermenuernes navigation bygger på data hentet fra en JSON fil, som indeholder bl.a
+          navnene på de navigationsknapper, der vises i menuen.
+        </p>
+        <p>Når der skal foretages ændringer i menuen, kan man nøjes med at opdatere titlerne i de relevante arrays i JSON filerne</p>
+        <p>Selve Header-komponenten håndtere automatisk både struktur og styling, så ændringerne slår igennem  uden yderligere justeringer
+          i komponentkoden.
+        </p>
+        <p>Denne tilgang gør det både hurtigt og sikkert at vedligeholde navigationen, samtidig med at koden forbliver overskuelig og
+          genanvendelig.
+        </p>
       </div>
     </div>
+    <div class="json__img">
+        <img :src="ManagePicture11" alt="jsonimg" id="jsonimage"> 
+      </div>
 
 
     <Footer>
@@ -43,26 +45,38 @@ import ManagePicture11 from "@/assets/image/manage-picture-11.webp";
 }
 
 .json__overall {
-  background-color: $color-anubis-black;
-  color: $color-newspaper-white;
-  width: 100%;
-  width: 30%;
+  display: flex;
+  flex-direction: column;
 }
 
 h2 {
-  margin: 10%;
+  display: flex;
+  margin: 2%;
 }
 
 p {
-  margin: 10%;
+  display: flex;
+  flex-direction: column;
+  margin: 2%;
+  width: 30%;
+}
+
+.json__text {
+  display: flex;
+  flex: 1;          
+  flex-direction: column;
 }
 
 .json__img {
+  flex: 0 0 auto;   
   display: flex;
+  justify-content: flex-end; 
+  align-items: center;       
 }
 
 img#jsonimage {
-  justify-content: center;
+  max-width: 100%;
+  height: auto;
 }
 
 .Footer__controls {
@@ -72,5 +86,6 @@ img#jsonimage {
 
 .footer__p {
   margin: 0%;
+  width: 100%;
 }
 </style>
