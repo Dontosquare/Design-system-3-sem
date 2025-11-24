5156fc191be6448f70ecb39668b0e29fae47292c
madsmads4 <madsolsen213@gmail.com>
2025-11-24 13:58:28 +0100

    Update HomeView.vue

--- designmanual/src/views/HomeView.vue
+++ designmanual/src/views/HomeView.vue
@@ -3,53 +3,55 @@ import Logo from "@/assets/image/faraos-logo.webp";
 </script>
 
 <template>
-  <div class="picture-wrapper">
-    <img :src="Logo" alt="Faraos logo" />
-  </div>
-  <div class="text__padding">
-    <h2>[Pharaos Cigarer - Webshop]</h2>
-    <p>Interaktiv Produktmanual</p>
-  </div>
-  <div class="text__padding">
-    <p>Version: v1.0.0 (20.11.2025)</p>
-    <p>Dokumenttype: Drift-, Opsætnings- og Vedligeholdelsesmanual</p>
-  </div>
-  <div class="text__padding">
-    <h2>Udviklet af</h2>
-    <p>Team 2:</p>
-    <ul>
-      <li>Joachim Pors Knudsen - Udvikler & scrummaster</li>
-      <li>Mads Grøndahl - Udvikler & designer</li>
-      <li>Sofie Abildgaard Pedersen - Teamleder, udvikler & lead designer</li>
-    </ul>
-  </div>
-  <div class="text__padding">
-    <h2>Kontakt:</h2>
-    <ul>
-      <li>Email: sape67086@edu.ucl.dk</li>
-    </ul>
-  </div>
-  <div class="text__padding">
-    <p>Institution: UCL - University College Lillebælt</p>
-    <p>Uddannelse: Multimediedesigner - Frontend Development</p>
-    <p>Semester: 3. semester - 2025</p>
-  </div>
-  <div class="footer-wrapper">
-    <div class="footer__link">
-      <p>Link til github</p>
-      <p>Link til prototype</p>
+  <main>
+    <div class="picture-wrapper">
+      <img :src="Logo" alt="Faraos logo" />
     </div>
-    <div class="footer__knap-dato">
-      <button>Start</button>
-      <p>Dato for sidst opdatering: d. 28 / 11 - 2025</p>
+    <div class="text__padding">
+      <h2>[Pharaos Cigarer - Webshop]</h2>
+      <p>Interaktiv Produktmanual</p>
     </div>
-  </div>
+    <div class="text__padding">
+      <p>Version: v1.0.0 (20.11.2025)</p>
+      <p>Dokumenttype: Drift-, Opsætnings- og Vedligeholdelsesmanual</p>
+    </div>
+    <div class="text__padding">
+      <h2>Udviklet af</h2>
+      <p>Team 2:</p>
+      <ul>
+        <li>Joachim Pors Knudsen - Udvikler & scrummaster</li>
+        <li>Mads Grøndahl - Udvikler & designer</li>
+        <li>Sofie Abildgaard Pedersen - Teamleder, udvikler & lead designer</li>
+      </ul>
+    </div>
+    <div class="text__padding">
+      <h2>Kontakt:</h2>
+      <ul>
+        <li>Email: sape67086@edu.ucl.dk</li>
+      </ul>
+    </div>
+    <div class="text__padding">
+      <p>Institution: UCL - University College Lillebælt</p>
+      <p>Uddannelse: Multimediedesigner - Frontend Development</p>
+      <p>Semester: 3. semester - 2025</p>
+    </div>
+    <div class="footer-wrapper">
+      <div class="footer__link">
+        <p>Link til github</p>
+        <p>Link til prototype</p>
+      </div>
+      <div class="footer__knap-dato">
+        <button>Start</button>
+        <p>Dato for sidst opdatering: d. 28 / 11 - 2025</p>
+      </div>
+    </div>
+  </main>
 </template>
 
 <style lang="scss" scoped>
 @import "../assets/main.scss";
 
-body {
+main {
   padding: 2rem;
   background: $color-pharaos-gold;
 }
