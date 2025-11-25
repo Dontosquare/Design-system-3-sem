ff3e68c2653e275cac8e8ad13fd79e03d96e7bf3
madsmads4 <madsolsen213@gmail.com>
2025-11-25 11:58:41 +0100

    Update TableOfContentView.vue

--- designmanual/src/views/TableOfContentView.vue
+++ designmanual/src/views/TableOfContentView.vue
@@ -1,6 +1,7 @@
 <script setup>
 import HeaderComp from "@/components/HeaderComp.vue";
 import FooterComp from "@/components/FooterComp.vue";
+import { RouterLink } from "vue-router";
 </script>
 
 <template>
@@ -9,20 +10,34 @@ import FooterComp from "@/components/FooterComp.vue";
     <h2>1. Introduktion</h2>
     <div class="text__padding">
       <h2>2. Opsætning</h2>
-      <h3>2.1 Installation</h3>
-      <h3>2.1.5 Trin for trin guide</h3>
-      <h3>2.2 Projektstruktur</h3>
-      <h3>2.3 Firebase</h3>
-      <h3>2.4 Framework</h3>
+      <RouterLink to="/setup-installation"
+        ><h3>2.1 Installation</h3></RouterLink
+      >
+      <RouterLink to="/setup-installation">
+        <h3>2.1.5 Trin for trin guide</h3></RouterLink
+      >
+      <RouterLink to="/setup-structure"
+        ><h3>2.2 Projektstruktur</h3></RouterLink
+      >
+      <RouterLink to="/setup-firebase"><h3>2.3 Firebase</h3></RouterLink>
+      <RouterLink to="/setup-framework"><h3>2.4 Framework</h3></RouterLink>
     </div>
     <div class="text__padding">
       <h2>3. Drift</h2>
-      <h3>3.1 Component based approach</h3>
-      <h3>3.2 Import af komponenter</h3>
-      <h3>3.3 Styling / SCSS</h3>
-      <h3>3.4 SPA-routing</h3>
-      <h3>3.5 Dynamisk navigation via JSON</h3>
-      <h3>3.5.5 Template og script til dynamisk navigation</h3>
+      <RouterLink to="/manage-component"
+        ><h3>3.1 Component based approach</h3></RouterLink
+      >
+      <RouterLink to="/manage-import"
+        ><h3>3.2 Import af komponenter</h3></RouterLink
+      >
+      <RouterLink to="/manage-styling"><h3>3.3 Styling / SCSS</h3></RouterLink>
+      <RouterLink to="/manage-routing"><h3>3.4 SPA-routing</h3></RouterLink>
+      <RouterLink to="/manage-JSON"
+        ><h3>3.5 Dynamisk navigation via JSON</h3></RouterLink
+      >
+      <RouterLink to="/manage-JSON-template"
+        ><h3>3.5.5 Template og script til dynamisk navigation</h3></RouterLink
+      >
     </div>
     <div class="text__padding">
       <h2>4. Vedligholdelse</h2>
