fb610db949f284869bb665d82c4fdde1208c6af0
Dontosquare <153940558+Dontosquare@users.noreply.github.com>
2025-11-25 10:20:20 +0100

    Update MaintenanceExpandView.vue

--- designmanual/src/views/MaintenanceExpandView.vue
+++ designmanual/src/views/MaintenanceExpandView.vue
@@ -1,6 +1,7 @@
 <script setup>
 import HeaderComp from '@/components/HeaderComp.vue';
 import FooterComp from '@/components/FooterComp.vue';
+import Promo from "@/assets/image/maintenance-picture.webp";
 </script>
 
 <template>
@@ -12,17 +13,17 @@ import FooterComp from '@/components/FooterComp.vue';
         <h3>Opdatering af indhold</h3>
         <p>Ændringer i tekster, billeder og logoer foretages direkte i de relevante komponenter. Eksempelvis opdateres logoet
            i Header.vue, hvor et nyt billede importeres, mens almindelige tekstændringer typisk sker i komponenternes <<span class="color-change">p</span>>-tag eller andre
-           tekstfelter.
-        </p>
-        <p>Selve Header-komponenten håndtere automatisk både struktur og styling, så ændringerne slår igennem  uden yderligere justeringer
-          i komponentkoden.
-        </p>
-        <p>Denne tilgang gør det både hurtigt og sikkert at vedligeholde navigationen, samtidig med at koden forbliver overskuelig og
-          genanvendelig.
-        </p>
+           tekstfelter.</p>
+
+        <h3>Globale stilændringer</h3>
+        <p>Farver og generelle stylingprincipper administreres centralt i main.css. Hvis man eksempelvis ønsker at udskifte en global
+          farve, tilpasses den blot her, hvorefter ændringen automatisk slår igennem i alle komponenter.</p>
+
+        <h3>Struktur for nye elementer</h3>
+        <p>For at udvide projektet følges denne faste struktur</p>
       </div>  
         <div class="json__img">
-        <img :src="ManagePicture11" alt="jsonimg" id="jsonimage"> 
+        <img :src="Promo" alt="promoimg" id="promoimage"> 
         </div>
     </div>
   </main>
@@ -60,7 +61,7 @@ p {
   align-items: center;       
 }
 
-img#jsonimage {
+img#promoimage {
   max-width: 100%;
   height: auto;
   margin-right: 10rem;
